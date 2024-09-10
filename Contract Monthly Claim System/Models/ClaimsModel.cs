a620cbb1027563dee121b3bfd60df7b4e071fcdd
James Knox <jamesknox361@gmail.com>
2024-09-10 07:59:02 +0200

    Switched to some bootstrap styling for home page content aswell as created new style for the welcome message. Fixed up navbar.

---
+++ Contract Monthly Claim System/Models/ClaimsModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_Claim_System.Models
+{
+    public class ClaimsModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
