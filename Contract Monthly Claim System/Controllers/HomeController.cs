0d6ffaebb35f8291d83a1650219aded648465794
James Knox <jamesknox361@gmail.com>
2024-09-10 14:12:25 +0200

    Added final references to all pages.

--- Contract Monthly Claim System/Controllers/HomeController.cs
+++ Contract Monthly Claim System/Controllers/HomeController.cs
@@ -1,3 +1,10 @@
+//James Knox
+//ST10048826
+//GROUP 3
+//References:
+//Bootstrap, n.d. CSS. [Online] Available at: https://getbootstrap.com/docs/3.4/css/#tables [Accessed 9 September 2024].
+//OpenAI.2024.Chat-GPT (Version 3.5).[Large language model].Available at: https://chat.openai.com/ [Accessed: 8 September 2024]
+
 using Contract_Monthly_Claim_System.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
--- Contract Monthly Claim System/Models/ClaimsModel.cs
+++ Contract Monthly Claim System/Models/ClaimsModel.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//James Knox
+//ST10048826
+//GROUP 3
+//References:
+//Bootstrap, n.d. CSS. [Online] Available at: https://getbootstrap.com/docs/3.4/css/#tables [Accessed 9 September 2024].
+//OpenAI.2024.Chat-GPT (Version 3.5).[Large language model].Available at: https://chat.openai.com/ [Accessed: 8 September 2024]
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace Contract_Monthly_Claim_System.Models
 {
